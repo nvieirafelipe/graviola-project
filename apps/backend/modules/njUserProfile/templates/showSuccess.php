a0710251a88076c0aa78d08eb97a9ad94e80a186
Felipe Vieira <nvieirafelipe@gmail.com>
2011-12-10 17:43:57 -0200

    Correções do mardegan, e Boxfile alterado para permitir upload de fotos do perfil

--- apps/backend/modules/njUserProfile/templates/showSuccess.php
+++ apps/backend/modules/njUserProfile/templates/showSuccess.php
@@ -1,5 +1,4 @@
-<a href="<?php echo url_for('njUserProfile/edit?id='.$nj_user_profile->getId()); ?>" class="link small edit_profile" title="Edit Profile">Edit Profile</a>
-<br />
+<a href="<?php echo url_for('njUserProfile/edit?id='.$sf_user->getProfile()); ?>" class="link small edit_profile" title="Edit Profile">Edit Profile</a><br />
 <div class="column box block">
   <?php if($nj_user_profile->getPicture()): ?>
     <?php echo image_tag(public_path(sfConfig::get('app_uploads_profile_pictures').$nj_user_profile->getPicture()), 
@@ -12,64 +11,121 @@
   <br />
 </div>
 <div class="column span-10 prepend-1">
-  <p>
-    User:
-    <?php echo $nj_user_profile->getSfGuardUser() ?>
-  </p>
-  <p>
-    First Name:
-    <?php echo $nj_user_profile->getSfGuardUser()->getFirstName(); ?>
-  </p>
-  <p>
-    Last Name:
-    <?php echo $nj_user_profile->getSfGuardUser()->getLastName(); ?>
-  </p>
-  <p>
-    E-mail:
-    <?php echo $nj_user_profile->getSfGuardUser()->getEmailAddress(); ?>
-  </p>
-  <p>
-    Username:
-    <?php echo $nj_user_profile->getSfGuardUser()->getUsername(); ?>
-  </p>
-  <p>
-    Address:
-    <?php echo $nj_user_profile->getAddress(); ?>
-  </p>
-  <p>
-    Complement:
-    <?php echo $nj_user_profile->getComplement(); ?>
-  </p>
-  <p>
-    District:
-    <?php echo $nj_user_profile->getDistrict(); ?>
-  </p>
-  <p>
-    City:
-    <?php echo $nj_user_profile->getCity(); ?>
-  </p>
-  <p>
-    State:
-    <?php echo $nj_user_profile->getState(); ?>
-  </p>
-  <p>
-    Country:
-    <?php echo $nj_user_profile->getCountry(); ?>
-  </p>
-  <p>
-    Web Page:
-    <?php echo $nj_user_profile->getWebpage(); ?>
-  </p>
+  <table class="user_profile">
+   <tr>
+     <th>
+       Username:
+     </th>
+     <th>
+        <p class="user_profile"><?php echo $nj_user_profile->getSfGuardUser() ?> </p>
+     </th>
+   </tr>
+   <tr>
+     <th>
+        First Name:
+     </th>
+     <th>
+        <p class="user_profile"><?php echo $nj_user_profile->getSfGuardUser()->getFirstName(); ?> </p>
+     </th>
+   </tr>
+   <tr>
+      <th>
+        Last Name:
+      </th>
+      <th>
+        <p class="user_profile"><?php echo $nj_user_profile->getSfGuardUser()->getLastName(); ?></p>
+      </th>
+   </tr>
+   <tr>
+      <th>
+         E-mail:
+      </th>
+      <th>
+         <p class="user_profile"><?php echo $nj_user_profile->getSfGuardUser()->getEmailAddress(); ?></p>
+      </th>
+   </tr>
+      <tr>
+      <th>
+         Phone:
+      </th>
+      <th>
+         <p class="user_profile"><?php echo $nj_user_profile->getPhone(); ?></p>
+      </th>
+   </tr>
+      <tr>
+      <th>
+         Cell:
+      </th>
+      <th>
+         <p class="user_profile"><?php echo $nj_user_profile->getCell(); ?></p>
+      </th>
+   </tr>
+   <tr>
+      <th>
+         Address:
+      </th>
+      <th>
+         <p class="user_profile"> <?php echo $nj_user_profile->getAddress(); ?></p>
+      </th>
+    </tr>
+    <tr>
+       <th>
+          Complement:
+       </th>
+       <th>
+          <p class="user_profile"><?php echo $nj_user_profile->getComplement(); ?></p>
+       </th>
+    </tr>
+    <tr>
+       <th>
+          District:
+       </th>
+       <th>
+         <p class="user_profile"><?php echo $nj_user_profile->getDistrict(); ?> </p>
+       </th>
+    </tr>
+    <tr>
+       <th>
+          City:
+       </th>
+       <th>
+          <p class="user_profile"><?php echo $nj_user_profile->getCity(); ?> </p>
+       </th>
+    </tr>
+    <tr>
+       <th>
+          State:
+       </th>
+       <th>
+          <p class="user_profile"><?php echo $nj_user_profile->getState(); ?> </p>
+       </th>
+    </tr>
+       <th>
+          Country:
+       </th>
+       <th>
+          <p class="user_profile"><?php echo $nj_user_profile->getCountry(); ?></p>
+       </th>
+    </tr>
+    <tr>
+       <th>
+          Web Page:
+       </th>
+       <th>
+          <p class="user_profile"><?php echo $nj_user_profile->getWebpage(); ?></p>
+       </th>
+       </tr>
+  </table>
 </div>
 <div class="column span-24">
-  <p>
-    Last Login:
+  <p class="user_profile">
+    Last login:
     <?php echo $nj_user_profile->getSfGuardUser()->getLastLogin(); ?>
   </p>
-  <p>
-    Profile created at:
+  <p class="user_profile">
+    Created at:
     <?php echo $nj_user_profile->getCreatedAt() ?>
-    , last updated at:
+    , Updated At:
     <?php echo $nj_user_profile->getUpdatedAt() ?>.
   </p>
 </div>
