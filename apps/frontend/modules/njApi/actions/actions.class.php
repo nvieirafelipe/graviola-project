b0b9b0ba9863c8d4180f8a701f0999f22d8ef08f
Felipe Vieira <nvieirafelipe@gmail.com>
2011-12-13 02:37:34 -0200

    Revert "Created json for Mobile project."
    This reverts commit af35f3847506302a682a92fb1158b16feab8c5f5.

--- apps/frontend/modules/njApi/actions/actions.class.php
+++ apps/frontend/modules/njApi/actions/actions.class.php
@@ -24,16 +24,10 @@ class njApiActions extends sfActions
     $trip_id = $request->getParameter('trip_id');
     
     foreach ($this->trips as $nj_trip) {
-      
       if ($nj_trip->getId() == $trip_id) {
         $current_trip = $nj_trip;
       }
     }
-    
-    if (empty($current_trip)) {
-      throw  new sfError404Exception('Trip not found');
-    }
-    
     
     foreach ($current_trip->getNjStopTimes() as $index => $stop_time) {
       $this->stop_time_coords[] = $stop_time->getNjStop()->getLatitude() . ';' . $stop_time->getNjStop()->getLongitude() . ';<strong>' . $stop_time->getNjStop()->getDescription().'</strong>';
@@ -43,15 +37,10 @@ class njApiActions extends sfActions
     $this->stop_time_coords = implode('|', $this->stop_time_coords);
   }
   
-  public function executeTransportList(sfWebRequest $request)
+  public function executeTransportFilter(sfWebRequest $request)
   {
-    if(!$this->transports = Doctrine_Core::getTable("njTransportMode")->findAll(DOCTRINE_CORE::HYDRATE_ARRAY)) {
-      throw new sfError404Exception('Transports not found.'); 
-    }
-    
-    $this->getResponse()->setContentType('text/javascript; charset=utf-8');
-    
-    return $this->renderText($this->symfony_to_js($this->transports));
+    if(!$this->routes = Doctrine_Core::getTable("njRoute")->findAll(DOCTRINE_CORE::HYDRATE_ARRAY))
+      throw new sfError404Exception('Routes not found.'); 
   }
 
   public function executeLogin(sfWebRequest $request)
@@ -70,61 +59,5 @@ class njApiActions extends sfActions
     }
     else
       throw new sfError404Exception('Invalid username.');
-  }
-  
-  public function executeRoutesList(sfWebRequest $request) {
-    
-    $transport_mode_id = $request->getParameter('transport_mode');
-    
-    if (!empty($transport_mode_id)) {
-      $routes = Doctrine_Core::getTable("njRoute")->findBy('nj_transport_mode_id', $transport_mode_id, DOCTRINE_CORE::HYDRATE_ARRAY);
-    }
-    else {
-      $routes = Doctrine_Core::getTable("njRoute")->findAll(DOCTRINE_CORE::HYDRATE_ARRAY);
-    }
-    
-    foreach ($routes as $index => $route) {
-      $routes[$index]['trips'] = Doctrine_Core::getTable("njTrip")->findBy('nj_route_id', $route['id'], DOCTRINE_CORE::HYDRATE_ARRAY);
-    }
-    
-    $this->getResponse()->setContentType('text/javascript; charset=utf-8');
-    
-    return $this->renderText($this->symfony_to_js($routes));
-  }
-  
-  
-  public function symfony_to_js($var) {
-    switch (gettype($var)) {
-      case 'boolean':
-        return $var ? 'true' : 'false'; // Lowercase necessary!
-      case 'integer':
-      case 'double':
-        return $var;
-      case 'resource':
-      case 'string':
-        return '"'. str_replace(array("\r", "\n", "<", ">", "&"),
-                                array('\r', '\n', '\x3c', '\x3e', '\x26'),
-                                addslashes($var)) .'"';
-      case 'array':
-        // Arrays in JSON can't be associative. If the array is empty or if it
-        // has sequential whole number keys starting with 0, it's not associative
-        // so we can go ahead and convert it as an array.
-        if (empty ($var) || array_keys($var) === range(0, sizeof($var) - 1)) {
-          $output = array();
-          foreach ($var as $v) {
-            $output[] = $this->symfony_to_js($v);
-          }
-          return '[ '. implode(', ', $output) .' ]';
-        }
-        // Otherwise, fall through to convert the array as an object.
-      case 'object':
-        $output = array();
-        foreach ($var as $k => $v) {
-          $output[] = $this->symfony_to_js(strval($k)) .': '. $this->symfony_to_js($v);
-        }
-        return '{ '. implode(', ', $output) .' }';
-      default:
-        return 'null';
-    }
   }
 }
